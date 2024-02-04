355e64c306eedf94826037c9e530a5b522c74d00
Pig Frown <pigfrown@protonmail.com>
2024-02-04 10:56:44 +0000

    Initial commit

---
+++ test/cdklocal-bug.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdklocalBug from '../lib/cdklocal-bug-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdklocal-bug-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdklocalBug.CdklocalBugStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
